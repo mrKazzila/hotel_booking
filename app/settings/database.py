34eb38c82f94682fef5094ff855b058366b27bd2
mrKazzila <mrkazzila@gmail.com>
2023-08-28 19:27:21 +0300

    fix: made db url as str

--- app/settings/database.py
+++ app/settings/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.settings.config import get_settings as settings
 
-engine = create_async_engine(url=settings().database_url)
+engine = create_async_engine(url=str(settings().database_url))
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
