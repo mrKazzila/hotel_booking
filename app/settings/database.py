2b584ef4d95f72a9be96dd5c5ce7de12abd0df20
mrKazzila <mrkazzila@gmail.com>
2023-08-28 16:44:35 +0300

    feat: add main database settings

---
+++ app/settings/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from app.settings.config import get_settings as settings
+
+engine = create_async_engine(url=settings().database_url)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase): pass
