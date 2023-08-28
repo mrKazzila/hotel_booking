b86bd8f8b72675412f15b79f238542761b7a18db
mrKazzila <mrkazzila@gmail.com>
2023-08-28 19:26:53 +0300

    feat: add new models

--- app/migrations/base.py
+++ app/migrations/base.py
@@ -1,2 +1,4 @@
 from app.settings.database import Base
-from app.hotels.models import Hotels
+from app.hotels.models import Hotels, Rooms
+from app.bookings.models import Bookings
+from app.users.models import Users
