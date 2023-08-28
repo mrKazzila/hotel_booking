6f1dc8d3dd1e3a38b5547c5f8a6faf27271f314b
mrKazzila <mrkazzila@gmail.com>
2023-08-28 23:38:52 +0300

    refactor: move base module to core folder

---
+++ app/core/base.py
@@ -0,0 +1,6 @@
+# Import all the models, so that Base has them before being
+# imported by Alembic
+from app.settings.database import Base  # noqa
+from app.hotels.models import Hotels, Rooms  # noqa
+from app.bookings.models import Bookings  # noqa
+from app.users.models import Users  # noqa
--- app/migrations/base.py
+++
@@ -1,4 +0,0 @@
-from app.settings.database import Base
-from app.hotels.models import Hotels, Rooms
-from app.bookings.models import Bookings
-from app.users.models import Users
