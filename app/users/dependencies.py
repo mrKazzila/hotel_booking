51a3effa6191ff3d8f3af3c2c98e90088f65ffca
mrKazzila <mrkazzila@gmail.com>
2023-08-30 14:24:21 +0300

    feat: add user dependencies

---
+++ app/users/dependencies.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from fastapi import Depends, Request
+from jose import jwt, JWTError
+
+from app.core.exceptions import (
+    ExpireTokenException,
+    EmptyTokenException,
+    IncorrectTokenFormatException,
+    UserNotFoundException,
+)
+from app.settings.config import get_settings as settings
+from app.users.services import UserServices
+
+
+def __get_access_token(request: Request):
+    if access_token := request.cookies.get('booking_access_token'):
+        return access_token
+
+    raise EmptyTokenException
+
+
+async def get_current_user(access_token: str = Depends(__get_access_token)):
+    try:
+        payload_data = jwt.decode(
+            token=access_token,
+            key=settings().SECRET_KEY,
+            algorithms=settings().ALGORITHM,
+        )
+    except JWTError:
+        raise IncorrectTokenFormatException
+
+    # TODO: refactor me
+    expire_time = payload_data.get('exp')
+    if not expire_time or int(expire_time) < datetime.utcnow().timestamp():
+        raise ExpireTokenException
+
+    user_id: int = int(payload_data.get('sub'))
+    if not user_id:
+        raise UserNotFoundException
+
+    if user := await UserServices.find_by_id(model_id=user_id):
+        return user
+
+    raise UserNotFoundException
