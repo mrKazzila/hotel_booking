6b1f8eb91eaff6787bac3b782d2d0e2d4435eb5d
mrKazzila <mrkazzila@gmail.com>
2023-08-30 14:21:34 +0300

    feat: add new router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from app.bookings.router import router as booking_router
+from app.users.router import router as users_router
 
 app = FastAPI()
+app.include_router(router=users_router)
 app.include_router(router=booking_router)
