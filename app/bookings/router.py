3d433d19c0759f7f00d475b90749091046b6b309
mrKazzila <mrkazzila@gmail.com>
2023-09-03 17:54:18 +0300

    feat: add scheme for endpoints

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,10 +1,8 @@
-from datetime import date
-
 from fastapi import APIRouter, Depends
 
-from app.bookings.schemas import SBooking, SUserBookings
+from app.bookings.schemas import SAddBooking, SBooking, SDeletedUserBooking, SUserBookings
 from app.bookings.services import BookingServices
-from app.core.exceptions import RoomCannotBeBookedException, BookingsNotFoundException
+from app.core.exceptions import BookingsNotFoundException, RoomCannotBeBookedException
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -25,17 +23,15 @@ async def get_user_bookings(user: Users = Depends(get_current_user)) -> list[SUs
 
 @router.post('')
 async def add_booking(
-        room_id: int,
-        date_from: date,
-        date_to: date,
+        data: SAddBooking,
         user: Users = Depends(get_current_user),
 ) -> SBooking:
     """Создает бронирование для пользователя."""
     if booking := await BookingServices.create_booking(
             user_id=user.id,
-            room_id=room_id,
-            date_from=date_from,
-            date_to=date_to,
+            room_id=data.room_id,
+            date_from=data.date_from,
+            date_to=data.date_to,
     ):
         return booking
 
@@ -43,7 +39,10 @@ async def add_booking(
 
 
 @router.delete('/{booking_id}')
-async def delete_user_booking_by_id(booking_id: int, user: Users = Depends(get_current_user)):
+async def delete_user_booking_by_id(
+        booking_id: int,
+        user: Users = Depends(get_current_user),
+) -> SDeletedUserBooking:
     """Удаляет бронь пользователя."""
     if deleted_book := await BookingServices.delete_user_booking_by_id(booking_id=booking_id, user_id=user.id):
         return deleted_book
@@ -52,7 +51,7 @@ async def delete_user_booking_by_id(booking_id: int, user: Users = Depends(get_c
 
 
 @router.delete('/')
-async def delete_user_bookings(user: Users = Depends(get_current_user)):
+async def delete_user_bookings(user: Users = Depends(get_current_user)) -> list[SDeletedUserBooking]:
     """Удаляет все брони пользователя."""
     if deleted_bookings := await BookingServices.delete_user_bookings(user_id=user.id):
         return deleted_bookings
