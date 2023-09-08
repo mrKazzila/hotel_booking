07caf419941fd0381892ffac6ac7940a09a1b7a9
mrKazzila <mrkazzila@gmail.com>
2023-09-08 18:30:29 +0300

    frat: add rooms model

--- app/core/base.py
+++ app/core/base.py
@@ -1,6 +1,7 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.settings.database import Base  # noqa
-from app.hotels.models import Hotels, Rooms  # noqa
+from app.hotels.models import Hotels  # noqa
+from app.rooms.models import Rooms  # noqa
 from app.bookings.models import Bookings  # noqa
 from app.users.models import Users  # noqa
