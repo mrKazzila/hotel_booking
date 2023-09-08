3f2060bcb64ac7d5ff465544cc7ce144d8422bf9
mrKazzila <mrkazzila@gmail.com>
2023-09-08 18:29:35 +0300

    chore: remove room model

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import mapped_column, Mapped
 
@@ -31,44 +31,5 @@ class Hotels(Base):
     )
     image_id: Mapped[int] = mapped_column(
         doc='Hotel image id',
-        type_=Integer,
-    )
-
-
-class Rooms(Base):
-    """Model for rooms."""
-
-    __tablename__ = 'rooms'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    hotel_id: Mapped[str] = mapped_column(ForeignKey("hotels.id"), nullable=False)
-
-    name: Mapped[str] = mapped_column(
-        doc='Room name',
-        type_=String(100),
-        nullable=False,
-    )
-    description: Mapped[str] = mapped_column(
-        doc='Room description',
-        type_=String(3000),
-        nullable=True,
-    )
-    price: Mapped[int] = mapped_column(
-        doc='Room price',
-        type_=Integer,
-        nullable=False,
-    )
-    services: Mapped[dict] = mapped_column(
-        doc='Room services',
-        type_=JSONB,
-        nullable=True,
-    )
-    quantity: Mapped[int] = mapped_column(
-        doc='Number of rooms',
-        type_=Integer,
-        nullable=False,
-    )
-    image_id: Mapped[int] = mapped_column(
-        doc='Room image id',
         type_=Integer,
     )
