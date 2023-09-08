3058f8ad99082263edab7314484876b10695bbdd
mrKazzila <mrkazzila@gmail.com>
2023-09-08 18:21:13 +0300

    feat: add endpoint for frontend

---
+++ app/pages/router.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, Request
+from fastapi.templating import Jinja2Templates
+from app.hotels.router import get_hotels
+
+router = APIRouter(
+    prefix='/pages',
+    tags=['Frontend'],
+)
+
+templates = Jinja2Templates(directory='app/templates')
+
+
+@router.get('/hotels')
+async def get_hotels_page(request: Request, hotels=Depends(get_hotels)):
+    return templates.TemplateResponse(
+        name='hotels.html',
+        context={
+            'request': request,
+            'hotels': hotels,
+        },
+    )
+
+
