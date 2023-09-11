f78a48558d5f8c255c91b59cef02c018608d9261
mrKazzila <mrkazzila@gmail.com>
2023-09-11 11:20:29 +0300

    feat: add email template

---
+++ app/core/email_templates.py
@@ -0,0 +1,26 @@
+from email.message import EmailMessage
+
+from pydantic import EmailStr
+from app.settings.config import settings
+
+
+def create_booking_confirmation_template(booking, email_to: EmailStr):
+    # info = await booking
+    print(f'{booking=}')
+    email = EmailMessage()
+
+    email['Subject'] = 'Booking confirmation'
+    email['From'] = settings().SMTP_USER
+    email['To'] = email_to
+
+    email.set_content(
+        f"""
+        <h1>Please confirm your booking!</h1>
+        <br>
+        
+        You booked hotel
+        """,
+        subtype='html'
+    )
+
+    return email
