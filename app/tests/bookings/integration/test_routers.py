0c69ccf76190081ea61c5b8a9aca83e5ce19077b
mrKazzila <mrkazzila@gmail.com>
2023-09-22 22:33:38 +0300

    test: add tests for booking

---
+++ app/tests/bookings/integration/test_routers.py
@@ -0,0 +1,31 @@
+import pytest
+from httpx import AsyncClient
+
+from app.tests.bookings.integration.parametrize_data import router_add_and_get_booking_data
+
+
+@pytest.mark.parametrize(
+    'room_id, date_from, date_to, book_count, status_code',
+    router_add_and_get_booking_data,
+)
+async def test_router_add_and_get_booking(
+        room_id: int,
+        date_from: str,
+        date_to: str,
+        book_count: int,
+        status_code: int,
+        authenticated_ac: AsyncClient,
+) -> None:
+    response = await authenticated_ac.post(
+        '/bookings',
+        json={
+            'room_id': room_id,
+            'date_from': date_from,
+            'date_to': date_to,
+        },
+    )
+
+    assert response.status_code == status_code
+
+    response_ = await authenticated_ac.get('/bookings')
+    assert len(response_.json()) == book_count
---
+++ app/tests/bookings/integration/test_services.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import pytest
+
+from app.bookings.services import BookingServices
+from app.tests.bookings.integration.parametrize_data import services_add_and_get_booking_data
+
+
+@pytest.mark.parametrize(
+    'user_id, room_id, date_from, date_to',
+    services_add_and_get_booking_data,
+)
+async def test_services_add_and_get_booking(
+        user_id: int,
+        room_id: int,
+        date_from: datetime,
+        date_to: datetime,
+) -> None:
+    new_booking = await BookingServices.create_booking(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=date_from,
+        date_to=date_to,
+    )
+
+    assert new_booking.user_id == user_id
+    assert new_booking.room_id == room_id
+
+    new_booking_exist = await BookingServices.find_by_id(new_booking.id)
+    assert new_booking_exist is not None
