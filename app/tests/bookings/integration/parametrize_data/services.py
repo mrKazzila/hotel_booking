fb3d562537a8ffc49fb7b1404056d4b269fd97b2
mrKazzila <mrkazzila@gmail.com>
2023-09-23 00:32:12 +0300

    test: add package for parametrize data

---
+++ app/tests/bookings/integration/parametrize_data/services.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+services_add_and_get_booking_data = [
+    # user_id, room_id, date_from, date_to
+    (1, 1, datetime.strptime('2024-07-10', '%Y-%m-%d'), datetime.strptime('2024-07-11', '%Y-%m-%d')),
+]
