7be03a3c555c8aa9887cb13a4513a6fdcd7c7f04
mrKazzila <mrkazzila@gmail.com>
2023-09-22 22:28:14 +0300

    feat: add simple validations for password

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,6 +1,19 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 
 
 class SUserAuth(BaseModel):
     email: EmailStr
     password: str
+
+    @field_validator('password')
+    def __validate_password_length(cls, v: str) -> str:
+        password_len = len(v)
+
+        if any([
+            password_len < 3,
+            v is None,
+            v.count(' ') > 0,  # whitespace
+        ]):
+            raise ValueError('Password is empty or less then 5')
+
+        return v
